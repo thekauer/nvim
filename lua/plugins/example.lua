47b42523202672a8a961475f4b59b20897c34624
Kauer András <andras.kauer@ibm.com>
2023-01-31 20:11:38 +0100

    feat: :tada: add debugprint

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -239,6 +239,21 @@ return {
         open_mapping = [[<C-t>]],
       })
     end
-  }
+  },
+  {
+    "andrewferrier/debugprint.nvim",
+    config = function()
+      require("debugprint").setup({
+        create_keymaps = false,
+        create_commands = false
+      })
 
+      vim.keymap.set("n", "gl", function()
+        return require('debugprint').debugprint({ variable = true })
+      end, { expr = true, desc = "log variable under cursor" })
+      vim.keymap.set("n", "gL", function()
+        return require('debugprint').debugprint({ above = true, variable = true })
+      end, { expr = true, desc = "log variable under cursor, above the line" })
+    end,
+  }
 }
